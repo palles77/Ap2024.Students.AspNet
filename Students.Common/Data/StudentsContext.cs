2c305938ef84bd7ed1414f50dba60206a87c298f
Lesław Pawlaczyk <leslaw.pawlaczyk@recogify.com>
2024-04-26 18:59:28 +0200

    Adding json storage

--- Students.Common/Data/StudentsContext.cs
+++ Students.Common/Data/StudentsContext.cs
@@ -1,46 +1,94 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Students.Common.Models;
+using System.Collections.Generic;
+using System.IO;
 
-namespace Students.Common.Data;
-
-public class StudentsContext : DbContext
+namespace Students.Common.Data
 {
-    public StudentsContext (DbContextOptions<StudentsContext> options)
-        : base(options)
+    public class StudentsContext : DbContext
     {
+        public StudentsContext(DbContextOptions<StudentsContext> options)
+            : base(options)
+        {
+            LoadDataFromJson();
+        }
 
+        public StudentsContext()
+        {
+            LoadDataFromJson();
+        }
+
+        public DbSet<Student> Student { get; set; } = default!;
+        public DbSet<Subject> Subject { get; set; } = default!;
+        public DbSet<StudentSubject> StudentSubject { get; set; } = default!;
+        public DbSet<Animal> Animal { get; set; }
+        public DbSet<Book> Book { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("StudentsContext");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentSubject>()
+                .HasKey(ss => new { ss.StudentId, ss.SubjectId });
+
+            modelBuilder.Entity<StudentSubject>()
+                .HasOne(ss => ss.Student)
+                .WithMany(s => s.StudentSubjects)
+                .HasForeignKey(ss => ss.StudentId);
+
+            modelBuilder.Entity<StudentSubject>()
+                .HasOne(ss => ss.Subject)
+                .WithMany(s => s.StudentSubjects)
+                .HasForeignKey(ss => ss.SubjectId);
+        }
+
+        public override int SaveChanges()
+        {
+            SaveDataToJson();
+            return base.SaveChanges();
+        }
+
+        private void LoadDataFromJson()
+        {
+            if (File.Exists("data.json"))
+            {
+                var jsonData = File.ReadAllText("data.json");
+                var data = JsonConvert.DeserializeObject<Data>(jsonData);
+
+                Student.AddRange(data.Students);
+                Subject.AddRange(data.Subjects);
+                StudentSubject.AddRange(data.StudentSubjects);
+                Animal.AddRange(data.Animals);
+                Book.AddRange(data.Books);
+            }
+        }
+
+        private void SaveDataToJson()
+        {
+            var data = new Data
+            {
+                Students = Student.Local,
+                Subjects = Subject.Local,
+                StudentSubjects = StudentSubject.Local,
+                Animals = Animal.Local,
+                Books = Book.Local
+            };
+
+            var jsonData = JsonConvert.SerializeObject(data);
+            File.WriteAllText("data.json", jsonData);
+        }
     }
 
-    public StudentsContext()
+    public class Data
     {
-    }
-
-    public DbSet<Student> Student { get; set; } = default!;
-    public DbSet<Subject> Subject { get; set; } = default!;
-    public DbSet<StudentSubject> StudentSubject { get; set; } = default!;
-
-    public DbSet<Animal> Animal { get; set; }
-    public DbSet<Book> Book { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseInMemoryDatabase("StudentsContext");
-    }
-
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<StudentSubject>()
-            .HasKey(ss => new { ss.StudentId, ss.SubjectId });
-
-        modelBuilder.Entity<StudentSubject>()
-            .HasOne(ss => ss.Student)
-            .WithMany(s => s.StudentSubjects)
-            .HasForeignKey(ss => ss.StudentId);
-
-        modelBuilder.Entity<StudentSubject>()
-            .HasOne(ss => ss.Subject)
-            .WithMany(s => s.StudentSubjects)
-            .HasForeignKey(ss => ss.SubjectId);
+        public IEnumerable<Student> Students { get; set; }
+        public IEnumerable<Subject> Subjects { get; set; }
+        public IEnumerable<StudentSubject> StudentSubjects { get; set; }
+        public IEnumerable<Animal> Animals { get; set; }
+        public IEnumerable<Book> Books { get; set; }
     }
 }
